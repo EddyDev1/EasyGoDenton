91881d91a39e28c736768b90606f41fcc0a7662f
Ed <125836816+EddyDev1@users.noreply.github.com>
2024-05-07 15:11:50 -0500

    Delete DentonMap-feature-redesign directory

--- DentonMap-feature-redesign/src/scripts/mapDatabaseWriteFunctions.js
+++
@@ -1,9 +0,0 @@
-/**
- * Handler  for map marker writes 
- * Handles all the information that is not included in the map
- * 
- * March 21, 2024
- * Alexander Evans
- */
-
-import { writeLocationToFirestore } from "../firebase/firebaseRepository";
