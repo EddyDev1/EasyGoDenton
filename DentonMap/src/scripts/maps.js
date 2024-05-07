b64e919c8064be9af23c8dbe0425d6e1e9df80c5
Ed <125836816+EddyDev1@users.noreply.github.com>
2024-05-07 16:42:45 -0500

    removed api
    google api key no longer shows

--- DentonMap/src/scripts/maps.js
+++ DentonMap/src/scripts/maps.js
@@ -67,7 +67,7 @@ export const initializeLocationsMap = (array) => {
  */
 export const loadMap = () => {
   const loader = new Loader({
-    apiKey: "AIzaSyDRk4DsR-vRdjPoVoUXZ35A6MZ2bXLx0Eo",
+    apiKey: "YOUR_API_KEY",
     version: "weekly",
   });
 
